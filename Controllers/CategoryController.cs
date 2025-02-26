8b9c251976695068b16fe8cec64da286eccb1016
AhmedmohamedAhmed90 <125593437+AhmedmohamedAhmed90@users.noreply.github.com>
2025-02-26 23:35:26 +0200

    finished Category Controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -66,27 +66,33 @@ namespace Cs_Hub.Controllers
             }
         }
 
-  /*      public async Task<IActionResult> AllCategories()
+        [HttpGet("all-categories")]
+     public async Task<IActionResult> AllCategories()
         {
-            var  category =await _DbContext.Categories.ToListAsync();
+            var  categories =await _DbContext.Categories.ToListAsync();
 
+            if (categories == null)
+            {
+                return NotFound(new {message="Can not get the categories"});
+            }
 
-            return RedirectToAction("Index", "Home");
+            return Ok(new {message="the Categories found",categories});
+            
         }
 
 
-
+        [HttpDelete("delete-category/{id}")]
         public async Task<IActionResult> DeleteCategory(int Id)
         {
             try
             {
                 
-                var category = await _DbContext.Categories.FirstOrDefaultAsync(x => x.CategoryID == Id);
+                var category = await _DbContext.Categories.FindAsync(Id);
 
                
                 if (category == null)
                 {
-                    return NotFound();
+                    return NotFound(new {message=" The Category Not Found"});
                 }
 
                 
@@ -96,15 +102,41 @@ namespace Cs_Hub.Controllers
                 await _DbContext.SaveChangesAsync();
 
                
-                return RedirectToAction("Index", "Home");
+            return Ok(new {message="the Category deleted Successfully"});
             }
             catch (Exception ex)
             {
              
-                return StatusCode(500, "Internal server error: " + ex.Message);
+                return BadRequest(new {message="Internal server error: " ,ex.Message});
             }
         }
-  */
+
+        [HttpPost("add-resource/{ResourceId}/{CategoryId}")]
+        public async Task<IActionResult> AddResourceToCategory(int ResourceId, int CategoryId)
+        {
+           var resource = await _DbContext.Resources.FindAsync(ResourceId);
+           var category = await _DbContext.Categories.FindAsync(CategoryId);
+
+           if (resource == null || category == null)
+            {
+              return NotFound("Resource or Category not found.");
+            }
+
+           var resourceCategory = new ResourceCategory
+            {
+            ResourceID = ResourceId,
+            CategoryID = CategoryId
+            };
+
+            _DbContext.ResourceCategories.Add(resourceCategory);
+            await _DbContext.SaveChangesAsync();
+
+            return Ok("Resource added to category successfully.");
+        }
+
+
+
+  
     }
   
 
