4c94fc2f5866dd4e02cc09a2f2277585b2c63c35
Fatma-Nazeih <158602521+Fatma-Nazeih@users.noreply.github.com>
2025-04-08 05:34:27 +0200

    Add files via upload

---
+++ Angular/angular-cs-hub/src/app/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  // Redirect to the login page
+  router.navigate(['/login']);
+  return false;
+};
