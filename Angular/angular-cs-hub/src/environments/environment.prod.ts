9dd7cb1769a99acce1bdb44560b6c77b2e6d61ce
Fatma-Nazeih <158602521+Fatma-Nazeih@users.noreply.github.com>
2025-04-08 05:31:49 +0200

    Add files via upload

---
+++ Angular/angular-cs-hub/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api', // In production, API is on the same domain
+  appName: 'CS Hub - Learning Platform'
+};
---
+++ Angular/angular-cs-hub/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000/api', // Default ASP.NET Core API development URL
+  appName: 'CS Hub - Learning Platform'
+};
